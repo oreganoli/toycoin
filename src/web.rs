64424f7f288cfd35f7003e5191c6638b93625631
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-05-24 11:08:15 +0200

    wrote HTTP methods + stubs

--- src/web.rs
+++ src/web.rs
@@ -1,6 +1,9 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex, MutexGuard},
+};
 
-use crate::block::{self, BlockchainError};
+use crate::block::{Blockchain, BlockchainError};
 use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
@@ -33,4 +36,35 @@ impl IntoResponse for BlockchainError {
         error_map.insert("error", error_text);
         (status_code, Json(error_map)).into_response()
     }
+}
+/// Mutable application state to be shared between HTTP requests.
+pub struct State {
+    blockchain: Arc<Mutex<Blockchain>>,
+}
+/// GET /pending
+pub async fn get_pending(Extension(state): Extension<State>) -> impl IntoResponse {
+    let pending = state.blockchain.lock().unwrap().pending();
+    Json(pending)
+}
+/// GET /balances
+pub async fn get_balances(Extension(state): Extension<State>) -> impl IntoResponse {
+    let balances = state.blockchain.lock().unwrap().balance();
+    Json(balances)
+}
+/// GET /chain
+pub async fn get_chain(Extension(state): Extension<State>) -> impl IntoResponse {
+    let chain = state.blockchain.lock().unwrap().chain();
+    Json(chain)
+}
+/// POST /commit
+pub async fn post_commit(Extension(state): Extension<State>) -> impl IntoResponse {
+    Json(state.blockchain.lock().unwrap().commit())
+}
+/// POST /guess
+pub async fn post_guess(Extension(state): Extension<State>) -> impl IntoResponse {
+    todo!();
+}
+/// POST /wire
+pub async fn post_wire(Extension(state): Extension<State>) -> impl IntoResponse {
+    todo!();
 }
